4850602de892927cea8b36722d4f359774d63918
ygorxds <ygordesa2010@hotmail.com>
2022-04-25 15:12:11 -0300

    create: conectando front e back

--- frontend/front/src/pages/Logon/index.js
+++ frontend/front/src/pages/Logon/index.js
@@ -1,30 +1,49 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-import './styles.css';
-
+import { Link, useHistory } from 'react-router-dom'
 import heroesImg from "../../assets/heroes.png";
 import logoImg from "../../assets/logo.svg";
 import { FiLogIn } from "react-icons/fi";
+import React, { useState } from "react";
+import api from "../../services/api"
+import './styles.css';
+
 
 export default function Logon() {
+
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+
+  async function hangleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push("/profile")
+
+    } catch (err) {
+      alert('Falha no login tente novamente');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be the Hero" />
-
-        <form>
+        <form onSubmit={hangleLogin}>
           <h1>Faça o seu Login</h1>
-
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
-
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="E02041" />
             Não tenho cadastro
           </Link>
         </form>
       </section>
-
       <img src={heroesImg} alt="heroes" />
     </div>
   );
--- frontend/front/src/pages/Register/index.js
+++ frontend/front/src/pages/Register/index.js
@@ -16,19 +16,16 @@ export default function Register() {
 
   async function handleRegister(e) {
     e.preventDefault();
-
     const data = {
       name, email, whatsapp, city, uf,
     };
+
     try {
-
-
       const response = await api.post('ongs', data)
-      alert(`Seu ID de acesso: ${response.data.id} anote em um lugar seguro`)
+      alert(`Seu ID de acesso: ${response.data.id}`)
       history.push('/');
     } catch (err) {
       alert('Erro no cadastro, atualize a pagina ou tente novamente mais tarde')
-
 
     }
 
@@ -37,21 +34,15 @@ export default function Register() {
   return (
     <div className="register-container">
       <div className="content">
-
         <section>
           <img src={logoImg} alt="Be the hero" />
-
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
-
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="E02041" />
             Já tenho cadastro
           </Link>
-
-
         </section>
-
         <form onSubmit={handleRegister}>
 
           <input
